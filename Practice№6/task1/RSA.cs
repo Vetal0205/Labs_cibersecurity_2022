5c7c0791d0b78e1d2e251a7f4c2a2e5f459c060f
Vetal0205 <vetal0205merkulov@gmail.com>
2022-11-04 16:42:30 +0100

    [@Vetal0205] Practice 6 task1 add

---
+++ Practice№6/task1/RSA.cs
@@ -0,0 +1,52 @@
+using System.Security.Cryptography;
+
+namespace Practice6_1
+{
+    public class RSA
+    {
+        private static RSAParameters _publicKey, _privateKey;
+        private readonly static string CspContainerName = "RsaContainer";
+        // In-Memory Keys
+        public static void AssignNewKey()
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                _publicKey = rsa.ExportParameters(false);
+                _privateKey = rsa.ExportParameters(true);
+            }
+        }
+        public static byte[] EncryptData(byte[] dataToEncrypt)
+        {
+            byte[] cypherBytes;
+            var cspParams = new CspParameters
+            {
+                KeyContainerName = CspContainerName,
+                Flags = CspProviderFlags.UseMachineKeyStore
+            };
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                // rsa.PersistKeyInCsp = true;
+                rsa.ImportParameters(_publicKey);
+                cypherBytes = rsa.Encrypt(dataToEncrypt, true);
+            }
+            return cypherBytes;
+        }
+        public static byte[] DecryptData(byte[] dataToDecrypt)
+        {
+            byte[] plainBytes;
+            var cspParams = new CspParameters
+            {
+                KeyContainerName = CspContainerName,
+                Flags = CspProviderFlags.UseMachineKeyStore
+            };
+            using (var rsa = new RSACryptoServiceProvider(cspParams))
+            {
+                // rsa.PersistKeyInCsp = true;
+                rsa.ImportParameters(_privateKey);
+                plainBytes = rsa.Decrypt(dataToDecrypt, true);
+            }
+            return plainBytes;
+        }
+    }
+}
---
+++ Practice№6/task1/task1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+using Practice6_1;
+using System.Diagnostics;
+
+namespace Practice6_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            byte[] origin = Encoding.Unicode.GetBytes("Mit-21 student");
+            RSA.AssignNewKey();
+            byte[] encr = RSA.EncryptData(origin);
+            byte[] decr = RSA.DecryptData(encr);
+        
+            Console.WriteLine($"Origin: {Encoding.Unicode.GetString(origin)}");
+            Console.WriteLine($"Encrypted: `{Encoding.Unicode.GetString(encr)}`");
+            Console.WriteLine($"Decrypted: {Encoding.Unicode.GetString(decr)}");
+        }
+    }
+}
